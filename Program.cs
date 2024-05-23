31bc70d060bfc61faeab63ce7dfda2ecc58fa5d1
Garrett <Garrett@YUP>
2024-05-23 14:40:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Week_3_challege_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word to check if it is a palindrome");
+
+            string isPalindrome = Console.ReadLine();
+            if (TestPalindrome(isPalindrome))
+            {
+                Console.WriteLine($"{isPalindrome} is a palindrome.");
+
+            }
+            else 
+            {
+                Console.WriteLine($"{isPalindrome} is not a palindrome.");
+            }
+            
+            static bool TestPalindrome(string palindrome) 
+                { 
+                    bool IsPalindrome = false;
+                    for (int i = 0; i < palindrome.Length/2; i++)
+                    {
+                        if (!(palindrome[i] == palindrome[palindrome.Length - 1 - i])) 
+                        {
+                            return false;
+                        }
+
+                    }
+                    return true;
+                }
+            {
+                
+                
+
+            }
+        }
+
+        
+
+    }
+}
